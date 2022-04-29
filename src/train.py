ab6d9545480fcd0044c6f803c2b7f08d12e4bffb
EgorTrushin <e.v.trushin@gmail.com>
2022-04-30 00:17:46 +0200

    Final refactoring

--- src/train.py
+++ src/train.py
@@ -1,7 +1,132 @@
 import torch
+import gc
 import time
+import torch.nn as nn
+from transformers import get_cosine_schedule_with_warmup, get_linear_schedule_with_warmup
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
 from .meter import AverageMeter
 from .utils import timeSince
+from .data import TrainDataset
+from .model import CustomModel
+from .valid import valid_fn
+from .utils import get_score
+
+
+def train_loop(folds, fold, device, LOGGER, CFG):
+
+    LOGGER.info(f"\n###### Fold {fold}")
+
+    train_folds = folds[folds["fold"] != fold].reset_index(drop=True)
+    valid_folds = folds[folds["fold"] == fold].reset_index(drop=True)
+    valid_labels = valid_folds["score"].values
+
+    train_dataset = TrainDataset(CFG, train_folds)
+    valid_dataset = TrainDataset(CFG, valid_folds)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=CFG["batch_size"],
+        shuffle=True,
+        num_workers=CFG["num_workers"],
+        pin_memory=False,
+        drop_last=False,
+    )
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=CFG["batch_size"],
+        shuffle=False,
+        num_workers=CFG["num_workers"],
+        pin_memory=False,
+        drop_last=False,
+    )
+
+    model = CustomModel(CFG, config_path=None, pretrained=True)
+    torch.save(model.config, CFG["output_dir"] + "config.pth")
+    model.to(device)
+
+    optimizer_parameters = get_optimizer_params(
+        model, encoder_lr=CFG["encoder_lr"], decoder_lr=CFG["decoder_lr"], weight_decay=CFG["weight_decay"]
+    )
+    optimizer = AdamW(
+        optimizer_parameters, lr=CFG["encoder_lr"], eps=CFG["eps"], betas=(CFG["betas"][0], CFG["betas"][1])
+    )
+
+    num_train_steps = len(train_loader) * CFG["epochs"]
+    scheduler = get_scheduler(CFG, optimizer, num_train_steps)
+
+    criterion = nn.BCEWithLogitsLoss(reduction="mean")
+
+    best_score = 0.0
+
+    for epoch in range(CFG["epochs"]):
+
+        start_time = time.time()
+
+        avg_loss = train_fn(fold, train_loader, model, criterion, optimizer, epoch, scheduler, device, CFG)
+        avg_val_loss, predictions = valid_fn(valid_loader, model, criterion, device, CFG)
+
+        score = get_score(valid_labels, predictions)
+
+        elapsed = time.time() - start_time
+
+        LOGGER.info(f"Epoch {epoch+1} - train_loss: {avg_loss:.4f}  val_loss: {avg_val_loss:.4f}  time: {elapsed:.0f}s")
+        LOGGER.info(f"Epoch {epoch+1} - Score: {score:.4f}")
+
+        if best_score < score:
+            best_score = score
+            LOGGER.info(f"Epoch {epoch+1} - Save Best Score: {best_score:.4f}")
+            torch.save(
+                {"model": model.state_dict(), "predictions": predictions},
+                CFG["output_dir"] + f"fold{fold}_best.pth",
+            )
+
+    predictions = torch.load(CFG["output_dir"] + f"fold{fold}_best.pth", map_location=torch.device("cpu"))[
+        "predictions"
+    ]
+    valid_folds["pred"] = predictions
+
+    torch.cuda.empty_cache()
+    gc.collect()
+
+    return valid_folds
+
+
+def get_scheduler(cfg, optimizer, num_train_steps):
+    if cfg["scheduler"] == "linear":
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer, num_warmup_steps=cfg["num_warmup_steps"], num_training_steps=num_train_steps
+        )
+    elif cfg["scheduler"] == "cosine":
+        scheduler = get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=cfg["num_warmup_steps"],
+            num_training_steps=num_train_steps,
+            num_cycles=cfg["num_cycles"],
+        )
+    return scheduler
+
+
+def get_optimizer_params(model, encoder_lr, decoder_lr, weight_decay=0.0):
+    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    optimizer_parameters = [
+        {
+            "params": [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay)],
+            "lr": encoder_lr,
+            "weight_decay": weight_decay,
+        },
+        {
+            "params": [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay)],
+            "lr": encoder_lr,
+            "weight_decay": 0.0,
+        },
+        {
+            "params": [p for n, p in model.named_parameters() if "model" not in n],
+            "lr": decoder_lr,
+            "weight_decay": 0.0,
+        },
+    ]
+    return optimizer_parameters
 
 
 def train_fn(fold, train_loader, model, criterion, optimizer, epoch, scheduler, device, CFG):
--- training.py
+++ training.py
@@ -1,141 +1,22 @@
 #!/usr/bin/env python3
 """Training script for U.S. Patent Phrase to Phrase Matching."""
 
-import gc
 import os
-import time
 import warnings
 
 import pandas as pd
 import torch
-import torch.nn as nn
+
 import yaml
-from torch.optim import AdamW
-from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, get_cosine_schedule_with_warmup, get_linear_schedule_with_warmup
+
+from transformers import AutoTokenizer
 from src.logger import get_logger
-from src.utils import seed_everything, get_cpc_texts, get_score, get_result, get_folds, get_max_len
-from src.data import TrainDataset
-from src.model import CustomModel
-from src.train import train_fn
-from src.valid import valid_fn
+from src.utils import seed_everything, get_cpc_texts, get_result, get_folds, get_max_len
+from src.train import train_loop
+
 
 warnings.filterwarnings("ignore")
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
-
-
-def train_loop(folds, fold):
-
-    LOGGER.info(f"\n###### Fold {fold}")
-
-    train_folds = folds[folds["fold"] != fold].reset_index(drop=True)
-    valid_folds = folds[folds["fold"] == fold].reset_index(drop=True)
-    valid_labels = valid_folds["score"].values
-
-    train_dataset = TrainDataset(CFG, train_folds)
-    valid_dataset = TrainDataset(CFG, valid_folds)
-
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=CFG["batch_size"],
-        shuffle=True,
-        num_workers=CFG["num_workers"],
-        pin_memory=False,
-        drop_last=False,
-    )
-    valid_loader = DataLoader(
-        valid_dataset,
-        batch_size=CFG["batch_size"],
-        shuffle=False,
-        num_workers=CFG["num_workers"],
-        pin_memory=False,
-        drop_last=False,
-    )
-
-    model = CustomModel(CFG, config_path=None, pretrained=True)
-    torch.save(model.config, CFG["output_dir"] + "config.pth")
-    model.to(device)
-
-    def get_optimizer_params(model, encoder_lr, decoder_lr, weight_decay=0.0):
-        no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-        optimizer_parameters = [
-            {
-                "params": [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay)],
-                "lr": encoder_lr,
-                "weight_decay": weight_decay,
-            },
-            {
-                "params": [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay)],
-                "lr": encoder_lr,
-                "weight_decay": 0.0,
-            },
-            {
-                "params": [p for n, p in model.named_parameters() if "model" not in n],
-                "lr": decoder_lr,
-                "weight_decay": 0.0,
-            },
-        ]
-        return optimizer_parameters
-
-    optimizer_parameters = get_optimizer_params(
-        model, encoder_lr=CFG["encoder_lr"], decoder_lr=CFG["decoder_lr"], weight_decay=CFG["weight_decay"]
-    )
-    optimizer = AdamW(
-        optimizer_parameters, lr=CFG["encoder_lr"], eps=CFG["eps"], betas=(CFG["betas"][0], CFG["betas"][1])
-    )
-
-    def get_scheduler(cfg, optimizer, num_train_steps):
-        if cfg["scheduler"] == "linear":
-            scheduler = get_linear_schedule_with_warmup(
-                optimizer, num_warmup_steps=cfg["num_warmup_steps"], num_training_steps=num_train_steps
-            )
-        elif cfg["scheduler"] == "cosine":
-            scheduler = get_cosine_schedule_with_warmup(
-                optimizer,
-                num_warmup_steps=cfg["num_warmup_steps"],
-                num_training_steps=num_train_steps,
-                num_cycles=cfg["num_cycles"],
-            )
-        return scheduler
-
-    num_train_steps = int(len(train_folds) / CFG["batch_size"] * CFG["epochs"])
-    scheduler = get_scheduler(CFG, optimizer, num_train_steps)
-
-    criterion = nn.BCEWithLogitsLoss(reduction="mean")
-
-    best_score = 0.0
-
-    for epoch in range(CFG["epochs"]):
-
-        start_time = time.time()
-
-        avg_loss = train_fn(fold, train_loader, model, criterion, optimizer, epoch, scheduler, device, CFG)
-        avg_val_loss, predictions = valid_fn(valid_loader, model, criterion, device, CFG)
-
-        score = get_score(valid_labels, predictions)
-
-        elapsed = time.time() - start_time
-
-        LOGGER.info(f"Epoch {epoch+1} - train_loss: {avg_loss:.4f}  val_loss: {avg_val_loss:.4f}  time: {elapsed:.0f}s")
-        LOGGER.info(f"Epoch {epoch+1} - Score: {score:.4f}")
-
-        if best_score < score:
-            best_score = score
-            LOGGER.info(f"Epoch {epoch+1} - Save Best Score: {best_score:.4f}")
-            torch.save(
-                {"model": model.state_dict(), "predictions": predictions},
-                CFG["output_dir"] + f"fold{fold}_best.pth",
-            )
-
-    predictions = torch.load(CFG["output_dir"] + f"fold{fold}_best.pth", map_location=torch.device("cpu"))[
-        "predictions"
-    ]
-    valid_folds["pred"] = predictions
-
-    torch.cuda.empty_cache()
-    gc.collect()
-
-    return valid_folds
 
 
 if __name__ == "__main__":
@@ -174,7 +55,7 @@ if __name__ == "__main__":
     oof_df = pd.DataFrame()
     for fold in range(CFG["n_fold"]):
         if fold in CFG["trn_fold"]:
-            _oof_df = train_loop(train, fold)
+            _oof_df = train_loop(train, fold, device, LOGGER, CFG)
             oof_df = pd.concat([oof_df, _oof_df])
             LOGGER.info(f"#### Fold {fold} result")
             get_result(_oof_df)
