6ecfd0696637934ac70b0d49f182a2466060f827
EgorTrushin <e.v.trushin@gmail.com>
2022-06-10 14:29:47 +0200

    SmoothFocalLoss is added

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import yaml
+import torch.nn.functional as F
 from pytorch_lightning.callbacks import ModelCheckpoint
 from scipy.stats import pearsonr
 from torch.utils.data import DataLoader, Dataset
@@ -265,6 +266,52 @@ def MSLELoss(yhat, y):
     return torch.mean((torch.log(yhat + 1) - torch.log(y + 1)) ** 2)
 
 
+class FocalLoss(nn.Module):
+    def __init__(self, reduction="none", alpha=1, gamma=2):
+        super().__init__()
+        self.reduction = reduction
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def forward(self, inputs, targets):
+        bce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
+        pt = torch.exp(-bce_loss)
+        loss = self.alpha * (1.0 - pt) ** self.gamma * bce_loss
+        if self.reduction == "none":
+            loss = loss
+        elif self.reduction == "sum":
+            loss = loss.sum()
+        elif self.reduction == "mean":
+            loss = loss.mean()
+        return loss
+
+
+class SmoothFocalLoss(nn.Module):
+    def __init__(self, reduction="none", alpha=1, gamma=2, smoothing=0.0):
+        super().__init__()
+        self.reduction = reduction
+        self.focal_loss = FocalLoss(reduction="none", alpha=alpha, gamma=gamma)
+        self.smoothing = smoothing
+
+    @staticmethod
+    def _smooth(targets: torch.Tensor, smoothing=0.0):
+        assert 0 <= smoothing < 1
+        with torch.no_grad():
+            targets = targets * (1.0 - smoothing) + 0.5 * smoothing
+        return targets
+
+    def forward(self, inputs, targets):
+        targets = SmoothFocalLoss._smooth(targets, self.smoothing)
+        loss = self.focal_loss(inputs, targets)
+        if self.reduction == "none":
+            loss = loss
+        elif self.reduction == "sum":
+            loss = loss.sum()
+        elif self.reduction == "mean":
+            loss = loss.mean()
+        return loss
+
+
 loss_dict = {
     "BCEWithLogitsLoss": nn.BCEWithLogitsLoss(),
     "MSELoss": nn.MSELoss(),
@@ -272,6 +319,7 @@ loss_dict = {
     "RMSELoss": RMSELoss,
     "RMSLELoss": RMSLELoss,
     "MSLELoss": MSLELoss,
+    "SmoothFocalLoss": SmoothFocalLoss(reduction="mean"),
 }
 
 
